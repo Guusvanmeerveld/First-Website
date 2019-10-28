e60c5594b796788c7df812a7a995419087a13e48
Guuq <50501321+Guuq@users.noreply.github.com>
2019-10-28 17:17:48 +0100

    Removed tawk

--- resources/javall.js
+++ resources/javall.js
@@ -6,16 +6,16 @@ addEventListener("load", function() {
     }
 })
 
-// Tawk
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5da48879f82523213dc72f8f/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+// // Tawk
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/5da48879f82523213dc72f8f/default';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
 
 // Console mark
 console.log('%cDoor Guus van Meerveld', "color:#9c9c9c; font-size:30px;");

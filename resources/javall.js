5f5f7e0fd27f442883d3cbe2384ccb147275a6f3
Guuq <50501321+Guuq@users.noreply.github.com>
2019-10-29 16:26:31 +0100

    29-10-2019 #1

--- resources/javall.js
+++ resources/javall.js
@@ -5,11 +5,3 @@ console.log('%cDoor Guus van Meerveld', "color:#9c9c9c; font-size:30px;");
 var shortcon = $("<link>", {rel :'shortcut icon',href:'/resources/img/favicon.ico'})
 
 $('head').append(shortcon)
-<<<<<<< HEAD
-
-// Https
-if (location.protocol == "http:" && location.port !== "3000" && location.host !== "guusvanmeerveld.online") {
-  location.protocol = "https:"
-}
-=======
->>>>>>> e28a9453f062af65383d1324fd83a161d4d22222

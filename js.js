2afe5da2dce88bff6f84176c4ccadf3d1a5ba591
Guuq <50501321+Guuq@users.noreply.github.com>
2019-11-20 19:49:31 +0100

    20-11 #3

--- js.js
+++ js.js
@@ -8,10 +8,16 @@ function showTime() {
   var days = ["Zondag","Maandag","Dinsdag","Woensdag","Donderdag","Vrijdag","Zaterdag"]
   var months = ["Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "December"]
   if (date.getMinutes() < 10) {
-    d("mins").innerHTML = "0" + date.getMinutes()
+    d("mins").innerHTML = "0" + date.getMinutes() + ":"
   }
   else {
-    d("mins").innerHTML = date.getMinutes()
+    d("mins").innerHTML = date.getMinutes() + ":"
+  }
+  if (date.getSeconds() < 10) {
+    d("secs").innerHTML = "0" + date.getSeconds()
+  }
+  else {
+    d("secs").innerHTML = date.getSeconds()
   }
   d("hours").innerHTML = date.getHours() + ":"
   d("date").innerHTML = days[date.getDay()] + " " + date.getDate() + " " + months[date.getMonth()];

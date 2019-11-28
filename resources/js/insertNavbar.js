98c07cb17039dbfa19986625337c172b020f464b
Guuq <50501321+Guuq@users.noreply.github.com>
2019-11-28 17:40:44 +0100

    Improved navbar

--- resources/js/insertNavbar.js
+++ resources/js/insertNavbar.js
@@ -4,16 +4,17 @@ var g = document.querySelector.bind(document);
 
 // Insert nav tag
 var navigation = c("nav")
-navigation.className = "navigation"
+  navigation.className = "navigation"
 var navbarList = c("ul")
-navbarList.className = "navbarList"
-navigation.appendChild(navbarList)
-document.body.appendChild(navigation)
+  navbarList.className = "navbarList"
+  navigation.appendChild(navbarList)
+  document.body.appendChild(navigation)
 
-// Opacity
-var opacity = c("div")
-opacity.className = "opacityMenu lowerIndex menu"
-document.body.appendChild(opacity)
+
+// // Opacity
+// var opacity = c("div")
+// opacity.className = "opacityMenu lowerIndex menu"
+// document.body.appendChild(opacity)
 
 // Hamburger Menu
 var hamburger = c("i")
@@ -49,43 +50,45 @@ function searchEnter(key) {
 // Create Navbar
 Navbar.forEach(i=>{
   var navList = c("li")
-  navList.className = "navList"
-  var navButton = c("button");
-    navButton.className = "navbarbutton"
-    navButton.innerHTML = "<i class='" + i.icon + " fa-lg'></i>" + i.name
+    navList.className = "navList"
+  var navLink = c("a");
+    navList.appendChild(navLink);
   if (i.link) {
-    var navLink = c("a");
     navLink.href = i.link
-    navLink.appendChild(navButton);
-    navButton = navLink
+  }
+  if (i.name) {
+    navLink.innerHTML = i.name
   }
   if (i.desc) {
-    navButton.title = i.desc
+    navLink.title = i.desc
   }
-  navList.appendChild(navButton)
+  if (i.icon) {
+    var navImg = c("img")
+    navImg.src = i.icon
+    navImg.className = "navImg"
+    navImg.alt = "img"
+    navLink.appendChild(navImg)
+  }
   var dropdownList = c("ul")
     dropdownList.className = "dropdownList"
     dropdownList.style = "display: none;"
   if (typeof i.dropdown == "object") {
         i.dropdown.forEach(d=>{
-          var dropdownListItem = c("li");
-          // Dropdown button
-          var dropdownButton = c("button");
-            dropdownButton.innerHTML = "<i class='" + d.icon + " fa-lg'></i>" + d.name
-            dropdownButton.className = "dropdownButton"
-          if (d.desc) {
-            dropdownButton.title = d.desc
-          }
           if (d.link) {
-            var link = c("a");
-            link.href = d.link
-            link.appendChild(dropdownButton);
-            dropdownButton = link
+            var dropdownListItem = c("li");
+              dropdownListItem.className = "dropdownLi"
+            var dropdownLink = c("a");
+              dropdownLink.href = d.link
+              dropdownLink.innerHTML = d.name
+            if (d.desc) {
+              dropdownLink.title = d.desc
+            }
+            dropdownListItem.appendChild(dropdownLink);
+          dropdownList.appendChild(dropdownListItem);
           }
-          dropdownListItem.appendChild(dropdownButton);
-        dropdownList.appendChild(dropdownListItem);
+          navList.appendChild(dropdownList)
       })
+      navLink.innerHTML = navLink.innerHTML + "<i class='fas fa-caret-down'></i>"
     }
-    navList.appendChild(dropdownList)
   navbarList.appendChild(navList);
 })
--- resources/js/navbar.json.js
+++ resources/js/navbar.json.js
@@ -1,30 +1,25 @@
 Navbar = [
   {
-    name: "Home",
+    icon: "/resources/img/favicon.ico",
     desc: "Ga naar de homepagina",
-    link: "/",
-    icon: "fas fa-home"
+    link: "/"
   },
   {
     name: "Playground",
     desc: "Test je html live!",
-    link: "/playground",
-    icon: "fas fa-code"
+    link: "/playground"
   },
   {
     name: "Contact",
     desc: "Neem contact met mij op",
-    link: "/contact",
-    icon: "fas fa-phone"
+    link: "/contact"
   },
   {
     name: "Games",
     desc: "Hier kan je spelletjes spelen",
-    icon: "fas fa-gamepad",
     dropdown: [
       {
         name: "Vuurjongen en Watermeisje 4",
-        icon: "fas fa-fire",
         link: "/vuurjongen-en-watermeisje-4"
       }
     ]

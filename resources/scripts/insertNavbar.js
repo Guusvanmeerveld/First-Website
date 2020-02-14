0ee3c51370b6c3c977de512f0bae3e1cb52c2af7
Guuq <50501321+Guuq@users.noreply.github.com>
2020-02-14 16:09:49 +0100

    Fixed weird github dupes

--- resources/scripts/insertNavbar.js
+++ resources/scripts/insertNavbar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:resources/scripts/insertNavbar.js
 var c = document.createElement.bind(document);
 var d = document.getElementById.bind(document);
 var g = document.querySelector.bind(document);
@@ -61,7 +60,6 @@ Navbar.forEach(i=>{
     }
   navbarList.appendChild(navList);
 })
-=======
 var c = document.createElement.bind(document);
 var d = document.getElementById.bind(document);
 var g = document.querySelector.bind(document);
@@ -78,50 +76,3 @@ var navbarList = c("ul")
 var hamburger = c("i")
 hamburger.className = "fas fa-bars fa-2x"
 navigation.prepend(hamburger)
-
-// Create Navbar
-Navbar.forEach(i=>{
-  var navList = c("li")
-    navList.className = "navList"
-  var navLink = c("a");
-    navList.appendChild(navLink);
-  if (i.link) {
-    navLink.href = i.link
-  }
-  if (i.name) {
-    navLink.innerHTML = i.name
-  }
-  if (i.desc) {
-    navLink.title = i.desc
-  }
-  if (i.icon) {
-    var navImg = c("img")
-    navImg.src = i.icon
-    navImg.className = "navImg"
-    navImg.alt = "img"
-    navLink.appendChild(navImg)
-  }
-  var dropdownList = c("ul")
-    dropdownList.className = "dropdownList"
-    dropdownList.style = "display: none;"
-  if (typeof i.dropdown == "object") {
-        i.dropdown.forEach(d=>{
-          if (d.link) {
-            var dropdownListItem = c("li");
-              dropdownListItem.className = "dropdownLi"
-            var dropdownLink = c("a");
-              dropdownLink.href = d.link
-              dropdownLink.innerHTML = d.name
-            if (d.desc) {
-              dropdownLink.title = d.desc
-            }
-            dropdownListItem.appendChild(dropdownLink);
-          dropdownList.appendChild(dropdownListItem);
-          }
-          navList.appendChild(dropdownList)
-      })
-      navLink.innerHTML = navLink.innerHTML + "<i class='fas fa-caret-down'></i>"
-    }
-  navbarList.appendChild(navList);
-})
->>>>>>> 4025d46f19bf398a874a71b3c44db54cf0560a4a:resources/js/insertNavbar.js
